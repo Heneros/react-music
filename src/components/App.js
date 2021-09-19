749505a1a6afc790efede96841fc0788fa6bec70
Heneros <punk60826@gmail.com>
2021-09-20 00:23:58 +0300

    display songs and duration

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import SongList from "./SongList";
+import SongDetail from "./SongDetail";
+
 
 const App = () => {
     return (
@@ -8,6 +10,9 @@ const App = () => {
       <div className="column eight wide">
         <SongList />
       </div>
+       <div  className="column eight wide">
+        <SongDetail />
+       </div>
     </div>  
     </div>  
     );
